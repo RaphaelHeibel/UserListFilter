4f558739cc0c361b95aabaac240830b7e52564d7
RaphaelHeibel <raphaelrheibel@gmail.com>
2024-10-30 14:22:32 -0300

    criação do projeto e import do angular-material

---
+++ src/app/angular-material/angular-material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  imports: [],
+  exports: [],
+})
+export class AngularMaterialModule { }
