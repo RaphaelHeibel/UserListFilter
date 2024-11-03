c881fddd267730c1749bf104bb5eb640534b8f32
RaphaelHeibel <raphaelrheibel@gmail.com>
2024-11-03 20:52:12 -0300

    adição dos components user-details e filter

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,11 +1,35 @@
 import { NgModule } from "@angular/core";
 
-import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+
+
 
 @NgModule({
-  imports: [],
-  exports: [],
+  imports: [
+    MatListModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+  ],
+  exports: [
+    MatListModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+  ],
+  providers: [provideNativeDateAdapter()],
 })
 export class AngularMaterialModule { }
